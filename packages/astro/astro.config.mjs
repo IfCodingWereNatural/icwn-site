36341d19b83dfe2c8283b1e746d4da87e7cd5ff8
Craig Blackburn <craig.blackburn@youscience.com>
2023-04-26 21:30:20 -0600

    remove demo astro site

--- packages/astro/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config'
-import aws from 'astro-sst/lambda'
-
-// https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  adapter: aws(),
-})
